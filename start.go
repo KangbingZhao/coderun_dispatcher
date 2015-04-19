e02e0fa39aa0bdde4c22de5c1cd7479d8ae0d4f2
Kangbing Zhao <zhaokangbing@hotmail.com>
2015-04-19 21:38:37 +0800

    已添加内存和CPU用量

--- start.go
+++ start.go
@@ -10,6 +10,7 @@ import (
 	// "net/url"
 	"os"
 	// "reflect"
+	"github.com/antonholmquist/jason"
 	"strconv"
 	"strings"
 	// "strconv"
@@ -57,13 +58,25 @@ func getInitialServerAddr() serverConfig { // get default server info from ./met
 }
 
 type serverStat struct {
-	cpuUsage     float32 //百分比
+	cpuUsage     float64 //百分比
 	cpuFrequency int     //kHz
 	cpuCore      int     //核心数
 
 	memUsageTotal float64 //内存容量，单位为Byte
 	memUsageHot   float64 //当前活跃内存量
 	memCapacity   float64 //内存总量
+
+}
+
+func subSubstring(str string, start, end int) string { //截取字符串
+	if start < 0 {
+		start = 0
+	}
+	if end > len(str) {
+		end = len(str)
+	}
+
+	return string(str[start:end])
 
 }
 
@@ -81,90 +94,42 @@ func getServerStats(serverList serverConfig) []serverStat { // get current serve
 		cadvisorUrl := "http://" + serverList.Server[index].Host + ":" + strconv.Itoa(serverList.Server[index].CAdvisorPort)
 		posturl := cadvisorUrl + "/api/v1.0/containers"
 
-		reqContent := "{\"num_stats\":1,\"num_samples\":0}"
+		reqContent := "{\"num_stats\":2,\"num_samples\":0}"
 		body := ioutil.NopCloser(strings.NewReader(reqContent))
 		client := &http.Client{}
 		req, _ := http.NewRequest("POST", posturl, body)
-		resq, err := client.Do(req)
+		resq, _ := client.Do(req)
 		defer resq.Body.Close()
 		data, _ := ioutil.ReadAll(resq.Body)
 		// fmt.Println(string(data), err)
 
-		//保存获取的服务器状态信息
-		var jsonEncode interface{}
-		err = json.Unmarshal(data, &jsonEncode)
-		if err != nil {
-			logger.Error(err)
-		}
-		tempStat, ok := jsonEncode.(map[string]interface{})
-		if ok {
-			fmt.Println("结果")
-			// fmt.Println(tempStat["stats"])
-			// md, _ := tempStat["stats"].(map[string]interface{})
-			// fmt.Println(md)
-			// fmt.Println(reflect.TypeOf(tempStat["stats"]["0"]))
-			md, ok := tempStat["stats"].([]interface{})
-			// fmt.Println(md)
-			for _, iiv := range md {
-				// fmt.Println(ii, "ii is ", iiv)
-				mm, _ := iiv.(map[string]interface{})
-				// fmt.Println(mm["memory"])
+		t, _ := jason.NewObjectFromBytes(data)
+		stats, _ := t.GetObjectArray("stats") //从cAdvisor获取的最近两个stat,1是最新的
+		// fmt.Println("len is ", len(stats))
+		t1, _ := stats[1].GetString("timestamp")
+		t2, _ := stats[0].GetString("timestamp")
+		// fmt.Println("timestamp1 is ", t1, "the timestamp 2 is", t2)
+		t1Time, _ := strconv.ParseFloat(subSubstring(t1, 17, 29), 64) //从秒开始，舍弃最后一个字母Z，不知道Z什么意思
+		t2Time, _ := strconv.ParseFloat(subSubstring(t2, 17, 29), 64)
+		// t2Time, _ := strconv.ParseFloat(subSubstring(t1, 17, 50), 64)
+		// fmt.Println("t1 time is ", t1Time)
+		intervalInNs := (t1Time - t2Time) * 1000000000 //单位是纳秒
+		// fmt.Println("interval is ", intervalInNs)
+		t1CPUUsage, _ := stats[1].GetFloat64("cpu", "usage", "total")
+		t2CPUUsage, _ := stats[0].GetFloat64("cpu", "usage", "total")
+		fmt.Println("tiCPU is ", t1CPUUsage, t2CPUUsage)
+		su[index].cpuUsage = (t1CPUUsage - t2CPUUsage) / intervalInNs
+		/*		if su[index].cpuUsage < 0 { //不知道为什么是负数，处理一下
+				su[index].cpuUsage = -su[index].cpuUsage
+			}*/
+		// fmt.Println("usage isw ", su[index].cpuUsage)
+		memoryUsageTotal, _ := stats[1].GetFloat64("memory", "usage")
+		memoryUsageWorking, _ := stats[1].GetFloat64("memory", "working_set")
+		su[index].memUsageTotal = memoryUsageTotal
+		su[index].memUsageHot = memoryUsageWorking
 
-				mn, _ := mm["memory"].(map[string]interface{})
-				fmt.Println("用量是", mn["usage"])
-				// su[index].memUsageTotal = mn["usage"]
-				// su[index].memUsageHot = mn["working_set"]
-
-				// mc,_ := mm[""]
-
-			}
-			fmt.Println("ok is ", ok)
-
-			/*			var t interface{}
-
-																																																																																																																																																																																																   			t, ok = md.(map[string]interface{})*/
-
-			fmt.Println("ok is ", ok)
-			/*			for k, v := range t {
-						switch v2 := v.(type) {
-						case string:
-							fmt.Println(k, " is string", v2)
-						case int:
-							fmt.Println(k, " is string", v2)
-						default:
-							fmt.Println(k, " is other ", v2)
-						}
-					}*/
-
-			// var ha interface{}
-			// fmt.Println("ha is ", ha.(interface{}))
-
-			fmt.Println("结果")
-			//应当从中获取cpu_usage和memUsage、mee_working_set
-		}
-
-		posturl2 := cadvisorUrl + "/api/v1.0/machine"
-		client2 := &http.Client{}
-		req2, _ := http.NewRequest("POST", posturl2, nil)
-		resq2, _ := client2.Do(req2)
-		defer resq2.Body.Close()
-		data2, _ := ioutil.ReadAll(resq2.Body)
-
-		var jsonEncode2 interface{}
-		err = json.Unmarshal(data2, &jsonEncode2)
-		if err != nil {
-			logger.Error(err)
-		}
-		tempStat2, _ := jsonEncode2.(map[string]interface{})
-		//从此处获取没内存总量
-		// fmt.Println("Machine信息")
-		// _, ok = tempStat2["memory_capacity"].(float64)
-		_, ok = (int64(tempStat2["num_cores"])).(int64)
-		fmt.Println(tempStat2["num_cores"], "类型是 ", ok)
-		// su[index].memCapacity = tempStat2["memory_capacity"]
-		// su[index].cpuCore = tempStat2["num_cores"]
-		// su[index].cpuFrequency = tempStat2["cpu_frequency_khz"]
-		// fmt.Println("body is ", reqContent)
+		// fmt.Println("me", memoryUsageWorking)
+		// su[index].memCapacity
 
 	} // end of loop
 	// fmt.Println("daowol")
@@ -225,4 +190,5 @@ func main() {
 	m.Post("/api/dispatcher/v1.0/container/create", dispatchContainer)
 
 	m.Run()
+
 }
