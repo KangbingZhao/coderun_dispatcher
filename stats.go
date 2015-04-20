08dd148fa0011bd7a5fd563268e29802259d5edc
Kangbing Zhao <zhaokangbing@hotmail.com>
2015-04-20 19:23:37 +0800

    test split, too complex

---
+++ stats.go
@@ -0,0 +1,227 @@
+/*
+*	get the stats of the servers and containers
+ */
+
+package coderun_dispatcher
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/Sirupsen/logrus"
+	"github.com/antonholmquist/jason"
+	"github.com/go-martini/martini"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var logger = logrus.New()
+var ContainerMemCapacity = float64(20971520)
+
+type containerAddr struct { // function dispatcherContainer will return this
+	ServerIP   string
+	ServerPost int
+}
+type serverConfig struct { // store data of ./metadata/config.json
+	Server []struct {
+		Host         string
+		DockerPort   int
+		CAdvisorPort int
+	}
+}
+
+func getInitialServerAddr() serverConfig { // get default server info from ./metadata/config.json
+	r, err := os.Open("./metadata/config.json")
+	if err != nil {
+		logger.Error(err)
+	}
+	decoder := json.NewDecoder(r)
+	var c serverConfig
+	err = decoder.Decode(&c)
+	if err != nil {
+		logger.Error(err)
+	}
+	for k, v := range c.Server {
+		fmt.Println(k, v.Host, v.DockerPort, v.CAdvisorPort)
+	}
+	return c
+
+}
+
+type serverStat struct {
+	cpuUsage        float64 //百分比
+	cpuFrequencyKHz int64   //kHz
+	cpuCore         int64   //核心数,-1表示服务器不在线
+
+	memUsageTotal float64 //内存容量，单位为Byte
+	memUsageHot   float64 //当前活跃内存量
+	memCapacity   float64 //内存总量
+}
+
+type containerStat struct {
+	cpuUsage      float64 //percent
+	memUsageTotal float64 //Byte
+	memeUsageHot  float64
+	memCapacity   float64
+}
+
+func subSubstring(str string, start, end int) string { //截取字符串
+	if start < 0 {
+		start = 0
+	}
+	if end > len(str) {
+		end = len(str)
+	}
+
+	return string(str[start:end])
+}
+
+func getServerStats(serverList serverConfig) []serverStat { // get current server stat
+
+	su := make([]serverStat, 0, 10)
+	for index := 0; index < len(serverList.Server); index++ {
+		var temp serverStat
+		su = append(su, temp)
+		if serverList.Server[index].Host == "" {
+			su[index].cpuCore = -1
+			continue
+		}
+
+		// su[index].CPUUsage = "CPU" + strconv.Itoa(index)
+		// su[index].MemUsage = "Mem" + strconv.Itoa(index)
+
+		cadvisorUrl := "http://" + serverList.Server[index].Host + ":" + strconv.Itoa(serverList.Server[index].CAdvisorPort)
+		posturl := cadvisorUrl + "/api/v1.0/containers"
+
+		reqContent := "{\"num_stats\":2,\"num_samples\":0}"
+		body := ioutil.NopCloser(strings.NewReader(reqContent))
+		client := &http.Client{}
+		req, _ := http.NewRequest("POST", posturl, body)
+		resq, _ := client.Do(req)
+		defer resq.Body.Close()
+		data, _ := ioutil.ReadAll(resq.Body)
+		// fmt.Println(string(data), err)
+
+		t, _ := jason.NewObjectFromBytes(data)
+		stats, _ := t.GetObjectArray("stats") //从cAdvisor获取的最近两个stat,1是最新的
+		// fmt.Println("len is ", len(stats))
+		t1, _ := stats[1].GetString("timestamp")
+		t2, _ := stats[0].GetString("timestamp")
+		// fmt.Println("timestamp1 is ", t1, "the timestamp 2 is", t2)
+		t1Time, _ := strconv.ParseFloat(subSubstring(t1, 17, 29), 64) //从秒开始，舍弃最后一个字母Z，不知道Z什么意思
+		t2Time, _ := strconv.ParseFloat(subSubstring(t2, 17, 29), 64)
+		// t2Time, _ := strconv.ParseFloat(subSubstring(t1, 17, 50), 64)
+		// fmt.Println("t1 time is ", t1Time)
+		intervalInNs := (t1Time - t2Time) * 1000000000 //单位是纳秒
+		// fmt.Println("interval is ", intervalInNs)
+		t1CPUUsage, _ := stats[1].GetFloat64("cpu", "usage", "total")
+		t2CPUUsage, _ := stats[0].GetFloat64("cpu", "usage", "total")
+		// fmt.Println("tiCPU is ", t1CPUUsage, t2CPUUsage)
+		su[index].cpuUsage = (t1CPUUsage - t2CPUUsage) / intervalInNs
+
+		memoryUsageTotal, _ := stats[1].GetFloat64("memory", "usage")
+		memoryUsageWorking, _ := stats[1].GetFloat64("memory", "working_set")
+		su[index].memUsageTotal = memoryUsageTotal
+		su[index].memUsageHot = memoryUsageWorking
+
+		posturl2 := cadvisorUrl + "/api/v1.0/machine"
+		client2 := &http.Client{}
+		req2, _ := http.NewRequest("POST", posturl2, nil)
+		resq2, _ := client2.Do(req2)
+		defer resq2.Body.Close()
+		data2, _ := ioutil.ReadAll(resq2.Body)
+
+		tt2, _ := jason.NewObjectFromBytes(data2)
+		num_cores, _ := tt2.GetInt64("num_cores")
+		cpu_frequency_khz, _ := tt2.GetInt64("cpu_frequency_khz")
+		mem_capacity, _ := tt2.GetFloat64("memory_capacity")
+		su[index].cpuCore = num_cores
+		su[index].cpuFrequencyKHz = cpu_frequency_khz
+		su[index].memCapacity = mem_capacity
+
+	} // end of loop
+	/*	for i := len(su) - 1; i >= 0; i-- { //删去corenum=-1的服务器
+		if su[i].cpuCore == -1 {
+			su = delete(su, su[i])
+		}
+	}*/
+	return su
+
+}
+
+func getValidContainerName(url string) []string { //返回字符串为docker名字,如/docker/0a69438e4……
+	// reqContent := "{\"num_stats\":2,\"num_samples\":0}"
+	// body := ioutil.NopCloser(strings.NewReader(reqContent))
+	client := &http.Client{}
+	req, _ := http.NewRequest("POST", url, nil)
+	resq, _ := client.Do(req)
+	defer resq.Body.Close()
+	data, _ := ioutil.ReadAll(resq.Body)
+
+	dataDecoded, _ := jason.NewObjectFromBytes(data)
+	containerList, _ := dataDecoded.GetObjectArray("subcontainers")
+	// var containerNameList []string
+	containerNameList := make([]string, 0, 50)
+	for i, v := range containerList {
+		containerNameList = append(containerNameList, "")
+		containerNameList[i], _ = v.GetString("name")
+	}
+	// containerNameList,_
+	// fmt.Println("list is ", containerNameList)
+	// return containerNameList
+	return containerNameList
+}
+func getContainerStat(serverUrl string, ContainerNameList []string) []containerStat { //serverUrl format is: http://server_ip:port
+	cs := make([]containerStat, 0, 50)
+	for index := 0; index < len(ContainerNameList); index++ {
+		var temp containerStat
+		cs = append(cs, temp)
+
+		posturl := serverUrl + "/api/v1.0/containers" + ContainerNameList[index]
+		// fmt.Println("posturl is ", posturl)
+		// continue
+		reqContent := "{\"num_stats\":2,\"num_samples\":0}"
+		body := ioutil.NopCloser(strings.NewReader(reqContent))
+		client := &http.Client{}
+		req, _ := http.NewRequest("POST", posturl, body)
+		resq, _ := client.Do(req)
+		defer resq.Body.Close()
+		data, _ := ioutil.ReadAll(resq.Body)
+
+		// fmt.Println("test")
+		t, _ := jason.NewObjectFromBytes(data)
+		// fmt.Println("t是神马", data)
+		stats, _ := t.GetObjectArray("stats") //从cAdvisor获取的最近两个stat,1是最新的
+		// fmt.Println("len is ", len(stats))
+		t1, _ := stats[1].GetString("timestamp")
+		t2, _ := stats[0].GetString("timestamp")
+		// fmt.Println("test")
+		// fmt.Println("timestamp1 is ", t1, "the timestamp 2 is", t2)
+		t1Time, _ := strconv.ParseFloat(subSubstring(t1, 17, 29), 64) //从秒开始，舍弃最后一个字母Z，不知道Z什么意思
+		t2Time, _ := strconv.ParseFloat(subSubstring(t2, 17, 29), 64)
+		// t2Time, _ := strconv.ParseFloat(subSubstring(t1, 17, 50), 64)
+		// fmt.Println("t1 time is ", t1Time)
+		intervalInNs := (t1Time - t2Time) * 1000000000 //单位是纳秒
+		// fmt.Println("test")
+		// fmt.Println("interval is ", intervalInNs)
+		t1CPUUsage, _ := stats[1].GetFloat64("cpu", "usage", "total")
+		t2CPUUsage, _ := stats[0].GetFloat64("cpu", "usage", "total")
+		// fmt.Println("tiCPU is ", t1CPUUsage, t2CPUUsage)
+		cs[index].cpuUsage = (t1CPUUsage - t2CPUUsage) / intervalInNs
+
+		memoryUsageTotal, _ := stats[1].GetFloat64("memory", "usage")
+		memoryUsageWorking, _ := stats[1].GetFloat64("memory", "working_set")
+
+		cs[index].memUsageTotal = memoryUsageTotal
+		cs[index].memeUsageHot = memoryUsageWorking
+		cs[index].memCapacity = ContainerMemCapacity
+		// fmt.Println("container cpu is ", cs[index].cpuUsage)
+
+	} // end of loop
+	// fmt.Println("containerstat is gotten")
+	// fmt.Println("len is ", len(ContainerNameList))
+	return cs
+
+}
